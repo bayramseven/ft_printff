7aca4852acdf36892d3dbe0e05da26e2fa6e47a7
Bayram <sevenbayramm@gmail.com>
2026-04-01 18:04:15 +0300

    printf fonksiyonunda kullancağım makroları derinlemesine öğrendim , kullandığım kaynakları notlarım.txt dosyayına kayıt ediyorum proje bitiminde reademe dosyası hazırladığımda kaynaklara ekleyeceğim.

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bayram-seven <bayram-seven@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 00:28:16 by bayram-seve       #+#    #+#             */
-/*   Updated: 2026/04/01 00:43:06 by bayram-seve      ###   ########.fr       */
+/*   Updated: 2026/04/01 17:58:36 by bayram-seve      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,40 @@
 
 int	ft_printf(const char *str, ...)
 {
-    
-    
-
+    va_list argm;
+    int i;
+    int total;
+    while (str[i])
+    {
+            write(1,&str[i],1);
+            i++;
+            total++;
+    }
 
 }
+
+
+
+
+    
+/*
+
+ilk olarak printf 
+    printf in akışı şu şekilde yamaya çalışacağım->
+    argümanlarımı tutabilecek bir va_list değişkeni oluşturacağım.
+    va_start diyerek 
+    
+    bir adet str değişkeni ve printf içindeki % 'nin sonrasındaki harfe gelene kadar olan yerleri kontrol etmek ve write yazdırmak için kullanacağım.
+    !!her write yazdığında karakter yazdırdıkça değişken oluştur ve  1 ekle çünkü orijinal fonskiyonda toplam kaç karakter bastığını söylüyor!!
+ 
+    burada str nin içinde % olması durumunu kontrol için while a koşul ekleyeceğim. % ye denk olduğu anda belirtmiş olduğum fonksiyona gidecek ve orada bir çıktı verecek.
+    fonsksiyona grdiğinde str i+1 diyerek % nin bir sağındaki harfin yazılmamasını sağlayacağım.
+    içeride yazdırmak istediğim aargümanı va_arg diyerek çağıracağım. 
+    va_end diyerek belleğimi temizleyeceğim.
+
+
+    
+    tabii bu makroları kullanabilmek için gerekli olan kütüphaneyi .h dosyama ekleyeceğim.
+
+
+*/
